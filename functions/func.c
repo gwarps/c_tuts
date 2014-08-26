1057e14062ce1ef58c0c9f5b526c89acf7b28517
baseplate <puneet.mir@gmail.com>
2014-08-26 23:47:14 +0530

    full solution updated

--- functions/func.c
+++ functions/func.c
@@ -1,4 +1,11 @@
 #include "func.h"
 
-int * squareArray(int *arr, int size) {
+int * squareArray(int arr[], int size) {
+   int result[size];
+   int i = 0;
+   for (int i = 0; i < size; i++) {
+      result[i] = arr[i] * arr[i];
+   }
+
+   return arr;
 }
